ea685180affd6c75015aefa716554e8260750c6a
Mahimaanand <47138963+Mahimaanand@users.noreply.github.com>
2020-11-28 15:08:54 +0800

    profile screen done

--- SpotifyTest/constants/images.js
+++ SpotifyTest/constants/images.js
@@ -1,7 +1,9 @@
 
 export const logo = require("../assets/images/Logo.png");
 export const spotify = require("../assets/images/spotify.png")
+export const dummyprofile = require("../assets/images/dummyprofile.png")
 export default {
     logo,
-    spotify
+    spotify,
+    dummyprofile
 }
--- SpotifyTest/src/screens/Profile/profile.js
+++ SpotifyTest/src/screens/Profile/profile.js
@@ -12,7 +12,31 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: COLORS.white
-    }
+    },
+    view: {
+        alignItems: "center",
+      },
+      logo: {
+        width: '50%',
+        height: '50%'
+      },
+      logoSpotify: {
+        flex: .15,
+        marginRight: 10
+      },
+    
+      button: {
+        flexDirection: "row",
+        backgroundColor: COLORS.spotifyGreen,
+        marginTop: SIZES.padding * 2,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 25,
+        width: 300,
+        elevation: 10
+      }
+
 }
 )
 
@@ -21,9 +45,27 @@ export default class ProfileScreen extends Component {
         return (
             <SafeAreaView style={styles.container}>
                 <LinearGradient
-                    colors={["#fbc2eb", "#a6c1ee"]}
+                    colors={["#7ec53a", "#acef6d"]}
                     style={{ flex: 1 }}
                 >
+                    <View style={{ ...styles.view, flex: 3, justifyContent: "flex-start", alignItems: "center", paddingTop: "20%" }}>
+                        <Image
+                            source={images.dummyprofile}
+                            resizeMode="cover"
+                            style={styles.logo}
+                            
+                        />
+                        <View style={{ alignItems: "center", marginHorizontal: SIZES.padding, justifyContent:"flex-start" }}>
+                            <Text style={{ color: COLORS.white, marginTop: SIZES.padding / 2, textAlign: "center", ...FONTS.h2 }}>Jane Doe</Text>
+                        </View>
+                    </View>
+                    <View style={{ ...styles.view, flex: 1, justifyContent: "flex-start", marginBottom: "12%" }}>
+                        
+                        <TouchableOpacity style={styles.button} activeOpacity={0.5}>
+                            
+                            <Text style={{ color: COLORS.white, ...FONTS.body3, fontWeight: "bold" }}>Logout</Text>
+                        </TouchableOpacity>
+                    </View>
                 </LinearGradient>
             </SafeAreaView>
         )
